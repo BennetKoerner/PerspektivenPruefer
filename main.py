5da9a4a10c9f0078b284b322bca00097de012e73
Bennet Körner <bennet.koerner@de.markant.com>
2023-08-08 15:40:01 +0200

    Using a GUI now, keyboard supported

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
-import glob
+import tkinter as tk
 import os
+from PIL import Image, ImageTk
 
-from PIL import Image
-import matplotlib.pyplot as plt
+class ImageViewer(tk.Tk):
+    def __init__(self, folder):
+        super().__init__()
+        self.title("Image Viewer")
+        self.images = [os.path.join(folder, file) for file in os.listdir(folder) if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+        self.current_index = 0
 
-bilder = glob.glob("./Bilder/*.png")
-print(bilder)
-for bild in bilder:
-    print(bild)
-    img = Image.open(bild)
-    plt.imshow(img)
-    plt.axis('off')  # Deaktivieren der Achsenbeschriftung
-    plt.show()
+        self.label = tk.Label(self)
+        self.label.pack(padx=10, pady=10)
 
-    # Tastatureingabe abfragen
-    user_input = input("Drücken Sie Enter, um fortzufahren, oder 'n', um das Bild zu löschen: ")
+        self.btn_previous = tk.Button(self, text="Previous", command=self.show_previous)
+        self.btn_previous.pack(side=tk.LEFT, padx=5, pady=5)
 
-    if user_input.lower() == 'n':
-        # Bild löschen
-        os.remove(bild)
-        print(f"Bild {bild} wurde gelöscht.")
+        self.btn_delete = tk.Button(self, text="Delete", command=self.delete_image)
+        self.btn_delete.pack(side=tk.BOTTOM, padx=5, pady=5)
+
+        self.btn_next = tk.Button(self, text="Next", command=self.show_next)
+        self.btn_next.pack(side=tk.RIGHT, padx=5, pady=5)
+
+        self.show_image()
+
+        self.bind("<Left>", lambda event: self.show_previous())
+        self.bind("<Right>", lambda event: self.show_next())
+        self.bind("<Down>", lambda event: self.delete_image())
+
+    def show_image(self):
+        if 0 <= self.current_index < len(self.images):
+            image = Image.open(self.images[self.current_index])
+            image = image.resize((400, 400), Image.BILINEAR)
+            photo = ImageTk.PhotoImage(image)
+            self.label.config(image=photo)
+            self.label.image = photo
+
+    def show_next(self):
+        if self.current_index < len(self.images) - 1:
+            self.current_index += 1
+            self.show_image()
+
+    def delete_image(self):
+        if 0 <= self.current_index < len(self.images):
+            os.remove(self.images[self.current_index])
+            del self.images[self.current_index]
+            if self.current_index == len(self.images):
+                self.current_index -= 1
+            self.show_image()
+
+    def show_previous(self):
+        if self.current_index > 0:
+            self.current_index -= 1
+            self.show_image()
+
+if __name__ == "__main__":
+    folder_path = "Images"
+    app = ImageViewer(folder_path)
+    app.mainloop()
